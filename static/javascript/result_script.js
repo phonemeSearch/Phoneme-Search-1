c15de018d7e5bbef2d432bc258a0f25003846c92
DaKode96 <78631343+DaKode96@users.noreply.github.com>
2021-06-07 10:55:43 +0200

    changed accent sensitiv checkbox label

--- static/javascript/result_script.js
+++
@@ -1,8 +0,0 @@
-/*lemmas = document.getElementsByClassName("lemma");
-for (var i=0; i<lemmas.length; i++) {
-    console.log(lemmas[i]);
-    marked = lemmas[i].children;
-    for (var i=0; i<marked.length; i++) {
-        console.log(marked[i]);
-    }
-}*/

e1f452a0e7b032570e9f5b8a21b36719fb3d755b
DaKode96 <78631343+DaKode96@users.noreply.github.com>
2021-06-29 13:43:44 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-order = {"b": 1, "c": 2, "d": 3}
-
-to_sort = ["cbd", "bc", "dc", "cd"]
-
-def sort_key():
-    return [order.get(char) for char in sort_key()]
-
-print(to_sort.sort(key=sort_key())) 
